5ae3943a9e6720888043971abb34c9cf2dd62d23
Mlowegene <genemlowe@gmail.com>
2024-07-31 11:12:25 +0300

    project setup

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@/styles/globals.css';
+
+export const metadata = {
+    title : "Promptopia",
+    description: "discover and share AI prompts"
+}
+
+function RootLayout({children}) {
+  return (
+    <html lang='en'>
+      <body>
+        <div className='main'>
+            <div className='gradient'></div>
+        </div>
+        <main className='app'>
+            {children}
+        </main>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout;
